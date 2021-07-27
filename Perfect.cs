f23c40b24b2fa8d73c3977abd1a72f3d19fff992
syedfaizz <syedfaizz45@gmail.com>
2021-07-27 12:12:13 +0530

    [faisal] Add:day-7

---
+++ Perfect.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Perfect NumberExamaple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 496;
+            int i = 1;
+            int total = 0;
+
+            for (i = 1; i < num; i++)
+            {
+                if (num % i == 0)
+                    total += i;
+            }
+            if (total == num)
+                Console.Write("{0} is a perfect number", num);
+            else
+                Console.Write("{0} is not a perfect number", num);
+        }
+    }
+}
